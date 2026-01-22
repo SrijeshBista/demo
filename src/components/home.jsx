828f64a654ec1def258ec5e3e7f9661966f90c48
Srijesh Bista <srijeshbista367@gmail.com>
2026-01-22 08:03:21 +0545

    first commit

---
+++ src/components/home.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+function App() {
+  const [step, setStep] = useState("form"); // for the form where the user enter the name 
+  const [name, setName] = useState(""); // this is the output
+
+  const [lastname, setLastName] = useState("");
+  const [ok , setok] = useState("[name,Age,feedback]");
+
+  const error =() =>{
+    if(name===""|| lastname===""){
+      alert("Please Enter Your First Name And Last Name");
+    }
+    else{
+      setStep("result");
+    }
+
+  }
+  return (
+    <div >
+      {step === "form" && (
+        <>
+          <h2>Form</h2>
+          <input
+            placeholder="Enter your first name"
+            onChange={(e) => setName(e.target.value)}
+          /><br /><br />
+          <input placeholder="Enter your last name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <br /><br />
+          <button onClick={error}>
+            Submit
+          </button>
+        </>
+      )}
+
+      {step === "result" && (
+        <>
+          <h2>Result</h2>
+          <p>Welcome Mr/Mrs  {name} {lastname} </p>
+          <button onClick={() => setStep("form")}>
+            Back
+          </button>
+
+
+        <input  value = {ok} onChange={(e) => setok(e.target.value) }/>
+
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
